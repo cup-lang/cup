35a5a053df4fe5733a6f95ddef44d7eba18312f6
Julek <31388661+jgrajewsky@users.noreply.github.com>
2020-08-29 15:46:47 +0200

    .cp to .cup

--- cmp.cmd
+++ cmp.cmd
@@ -1,7 +1,7 @@
 cd C:\MinGW\bin
 gcc C:\Projects\cup\src\cup.c -o C:\Projects\cup\bin\cup.exe -O3 -s
 cd C:\Projects\cup
-bin\cup.exe test\test0\main.cp -o test\test0\main.c
+bin\cup.exe test\test0\main.cup -o test\test0\main.c
 
 REM cl src\cup.c /O2 /Fe:bin\cup.exe
-REM bin\cup.exe test\test0\main.cp -o test\test0\main.c
+REM bin\cup.exe test\test0\main.cup -o test\test0\main.c
