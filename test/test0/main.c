fdaf9ac08da2d8d84eb1e20e53f2056a48cc3ccb
Julek <31388661+jgrajewsky@users.noreply.github.com>
2020-09-13 00:30:04 +0200

    still ranting

--- test/test0/main.c
+++ test/test0/main.c
@@ -1,14 +1,6 @@
 #include <stdint.h>
-void main(){}
 
-void some_func() {
-    int a = 5;
-    a += a;
-    addr(a)
-}
-
-void some_func() {
-    int a = 5;
-    a += a;
-    return addr(a);
+void main()
+{
+    int a = 5 ? 1 : 0;
 }
