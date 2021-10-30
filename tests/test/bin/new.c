5c1f53e48a6e896c030bf2aec1659af88cf001e2
jgrajewsky <jgrajewsky@gmail.com>
2021-10-30 19:33:10 +0200

    .

--- tests/test/bin/new.c
+++ tests/test/bin/new.c
@@ -1,1 +1,1 @@
-typedef struct  ; ; ;typedef struct {};
+typedef struct _1 _1;typedef struct _5 _5;typedef struct _3 _3;typedef struct _4 _4;void _2(_6* this);typedef struct _1{}_1;typedef struct _3{}_3;typedef struct _4{}_4;typedef struct _5{int t;union{_3 _5;_4 _5;}u;}_5;void _2(_6* this){''};
