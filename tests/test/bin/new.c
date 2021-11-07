f7a73ff281a6b3ad97999578afafd62e9bf359bd
jgrajewsky <jgrajewsky@gmail.com>
2021-11-07 02:49:55 +0100

    .

--- tests/test/bin/new.c
+++ tests/test/bin/new.c
@@ -1,2 +1,3 @@
+#include "stdint.h"
 #include "stdio.h"
-typedef struct _1 _1;void main();typedef struct _1{}_1;void main(){printf("Hello, World!");};
+typedef struct _2 _2;void main();typedef struct _2{_4* buf;int32_t len;}_2;void main(){uint32_t __1='1';int8_t __2=1;int32_t __3=1;{int32_t __4;__4=(1+1);}printf("%i",__3);};
