79eac022478dce6bb7992b2ef562e2041af1b8e7
jgrajewsky <jgrajewsky@gmail.com>
2021-11-25 16:11:19 +0100

    .

--- tests/test/bin/new.c
+++ tests/test/bin/new.c
@@ -1,3 +1,3 @@
 #include "stdint.h"
 #include "stdio.h"
-typedef struct _2 _2;typedef struct _5 _5;typedef struct _6 _6;void main();typedef struct _2{uint32_t* buf;int32_t len;}_2;typedef struct _5{double* buf;int32_t len;}_5;typedef struct _6{float* buf;int32_t len;}_6;void main(){double __1;_5 __2;_6 __3;_5 __4;};
+typedef struct _2 _2;typedef struct _5 _5;int32_t _4();void main();typedef struct _2{uint32_t* buf;int32_t len;}_2;typedef struct _5{int32_t a;}_5;int32_t _4(){};void main(){_4();};
